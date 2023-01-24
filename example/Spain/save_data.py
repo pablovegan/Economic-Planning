360703aa6926a82f83cc103106b1f847f710a6c4
pablovegan <pablovegan98@icloud.com>
2023-01-24 19:57:27 +0100

    separate constraints in different functions

---
+++ example/Spain/save_data.py
@@ -0,0 +1,99 @@
+"""Save the supply-use tables and other data needed for planning."""
+
+from os.path import join
+from pickle import dump
+
+from numpy import eye
+from pandas import DataFrame, Series, read_excel, concat
+from scipy.sparse import csr_matrix
+
+
+def load_excel(
+    sheet_path: str, sheet_name: str, min_row: int, min_col: int, max_row: int, max_col: int
+) -> DataFrame:
+    """Load an excel sheet given the rows and columns."""
+    df = read_excel(
+        sheet_path,
+        sheet_name=sheet_name,
+        usecols=range(min_col - 1, max_col),
+        skiprows=range(min_row - 2),
+        nrows=max_row - min_row + 1,
+    )
+    return df.to_numpy()
+
+
+if __name__ == "__main__":
+
+    print("Starting...")
+
+    use_imported, supply_use = [], []
+    export_prices, import_prices = [], []
+    export_output, target_output = [], []
+    worked_hours = []
+
+    excel_names = ["cne_tod_16_en", "cne_tod_17_en", "cne_tod_18_en", "cne_tod_19_en"]
+    sheet_names = ["Table1", "Table3"]
+
+    for name in excel_names:
+        excel_path = join("example", "Spain", "data", name + ".xlsx")
+
+        supply_matrix = csr_matrix(load_excel(excel_path, "Table1", 10, 3, 123, 83))
+
+        use_matrix = csr_matrix(load_excel(excel_path, sheet_name, 4, 3, 63, 61))
+
+        supply_use.append(supply_matrix - use_matrix)
+
+        export_prices.append(load_excel(excel_path, sheet_name, 4, 80, 63, 80).flatten())
+        import_prices.append(load_excel(excel_path, sheet_name, 4, 80, 63, 80).flatten())
+
+        target_output.append(load_excel(excel_path, sheet_name, 4, 79, 63, 79).flatten())
+        export_output.append(load_excel(excel_path, sheet_name, 4, 78, 63, 78).flatten())
+        worked_hours.append(load_excel(excel_path, sheet_name, 69, 3, 69, 61).flatten())
+
+    # ! Depreciation matrix != Id may lead to infeasible solutions
+    depreciation = 0.95 * csr_matrix(eye(supply_use[0].shape[0]))
+    depreciation[59, 59] = 1  # Suppose CO2 is not reabsorbed
+    for i in range(27, 59):
+        depreciation[i, i] = 0.3  # Human services cannot be stored for the next period
+
+    # Interpolate years to have more data
+    for i in range(len(sheet_names) - 1):
+        idx = 2 * i + 1
+        supply_use.insert(idx, (supply_use[i + 1] + supply_use[i]) / 2)
+        use_imported.insert(idx, (use_imported[i + 1] + use_imported[i]) / 2)
+        export_prices.insert(idx, (export_prices[i + 1] + export_prices[i]) / 2)
+        import_prices.insert(idx, (import_prices[i + 1] + import_prices[i]) / 2)
+        export_output.insert(idx, (export_output[i + 1] + export_output[i]) / 2)
+        target_output.insert(idx, (target_output[i + 1] + target_output[i]) / 2)
+        worked_hours.insert(idx, (worked_hours[i + 1] + worked_hours[i]) / 2)
+
+    economy = {}
+
+    economy["supply_use"] = supply_use
+    economy["use_imported"] = use_imported
+    economy["export_prices"] = export_prices
+    economy["import_prices"] = import_prices
+    economy["export_output"] = export_output
+    economy["target_output"] = target_output
+    economy["depreciation"] = depreciation
+    economy["worked_hours"] = worked_hours
+
+    with open(join("example", "data", "swedish_economy.pkl"), "wb") as file:
+        dump(economy, file)
+
+    # Now we save the names of each product/sector
+    excel_path = join("example", "data", "posternas_namn.xlsx")
+    product_names = read_excel(
+        excel_path,
+        sheet_name="SUP10",
+        usecols=range(3 - 1, 3),
+        skiprows=range(7 - 2),
+        nrows=65 - 7 + 1,
+    )
+    # Convert pandas dataframe to series
+    product_names = product_names.squeeze()
+    product_names = concat([product_names, Series(["CO2"])], ignore_index=True)  # add CO2
+    with open(join("example", "data", "swedish_product_names.pkl"), "wb") as file:
+        dump(product_names, file)
+
+    print("Finished.")
